1ce7493bbbec39231402635705432900d4f502c6
Kevin <kevin.gilliard8415@gmail.com>
2018-03-06 20:24:55 -0500

    Update test code with new coding standard

--- test/AnalyticsE2ETest.php
+++ test/AnalyticsE2ETest.php
@@ -1,30 +1,33 @@
 <?php
 
-phpinfo();
-
-require_once(dirname(__FILE__) . "/../lib/Segment.php");
+require_once __DIR__ . "/../lib/Segment.php";
 
 class AxiosClient
 {
-  private $BaseAddress = null;
+  private $BaseAddress;
   private $Timeout = 3000;
   private $Authorization = "";
   private $RetryCount = 1;
 
-  public function __construct($baseAddress, $timeout, $authorization) {
+  public function __construct($baseAddress, $timeout, $authorization)
+  {
     $this->BaseAddress = $baseAddress ? $baseAddress : "https://api.runscope.com";
-    if ($timeout)
+    if ($timeout) {
       $this->Timeout = $timeout;
-    if ($authorization)
+    }
+    if ($authorization) {
       $this->Authorization = $authorization;
+    }
   }
 
-  public function setRetryCount($count) {
+  public function setRetryCount($count)
+  {
     $this->RetryCount = $count > 0 ? $count : 1;
   }
 
-  public function get($url) {
-    for ($i = 0; $i < $this->RetryCount; $i++) {
+  public function get($url)
+  {
+    for ($i = 0; $i < $this->RetryCount; ++$i) {
       // open connection
       $ch = curl_init();
 
@@ -42,46 +45,35 @@ class AxiosClient
 
       // Set url
       curl_setopt($ch, CURLOPT_URL, $this->BaseAddress . "/" . $url);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
       $output = curl_exec($ch);
       $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
       curl_close($ch);
 
-      if ((int)$response == 200)
+      if (200 == (int) $response) {
         return $output;
-
+      }
       usleep(3);
     }
 
-    return NULL;
+    return null;
   }
 }
 
-class AnalyticsE2ETest extends PHPUnit_Framework_TestCase {
-
+class AnalyticsE2ETest extends PHPUnit\Framework\TestCase
+{
   private static $WRITE_KEY = "OnMMoZ6YVozrgSBeZ9FpkC0ixH0ycYZn";
   private static $RUNSCOPE_BUCKET = "bmbbsjxdttr7";
   private $id = "";
   private $test_e2e;
 
-  private static function messageId(){
-    return sprintf("%04x%04x%04x%04x%04x%04x%04x%04x"
-      , mt_rand(0, 0xffff)
-      , mt_rand(0, 0xffff)
-      , mt_rand(0, 0xffff)
-      , mt_rand(0, 0x0fff) | 0x4000
-      , mt_rand(0, 0x3fff) | 0x8000
-      , mt_rand(0, 0xffff)
-      , mt_rand(0, 0xffff)
-      , mt_rand(0, 0xffff));
-  }
-
-  function setUp() {
-    $this->test_e2e = isset($_SERVER["RUN_E2E_TESTS"]) && (!!json_decode($_SERVER["RUN_E2E_TESTS"]));
-    if (!$this->test_e2e)
+  public function setUp()
+  {
+    $this->test_e2e = isset($_SERVER["RUN_E2E_TESTS"]) && ((bool) json_decode($_SERVER["RUN_E2E_TESTS"]));
+    if (!$this->test_e2e) {
       return;
-
+    }
     date_default_timezone_set("UTC");
 
     $this->id = self::messageId();
@@ -93,7 +85,7 @@ class AnalyticsE2ETest extends PHPUnit_Framework_TestCase {
       "userId" => "prateek",
       "event" => "Item Purchased",
       "property" => array(
-        "id" => $this->id,
+      "id" => $this->id,
       )
     ));
     Segment::flush();
@@ -102,10 +94,11 @@ class AnalyticsE2ETest extends PHPUnit_Framework_TestCase {
     sleep(5);     // 5 seconds.
   }
 
-  function testE2E() {
-    if (!$this->test_e2e)
+  public function testE2E()
+  {
+    if (!$this->test_e2e) {
       return;
-
+    }
     // Verify RUNSCOPE_TOKEN is defined as system variable
     $this->assertTrue(isset($_SERVER["RUNSCOPE_TOKEN"]));
     $token = $_SERVER["RUNSCOPE_TOKEN"];
@@ -113,18 +106,17 @@ class AnalyticsE2ETest extends PHPUnit_Framework_TestCase {
     $client = new AxiosClient("https://api.runscope.com", 10 * 1000, $token);
     $client->setRetryCount(3);
 
-    for ($i = 0; $i < 5; $i++)
-    {
+    for ($i = 0; $i < 5; ++$i) {
       // Runscope Bucket for https://www.runscope.com/stream/bmbbsjxdttr7.
       $messageResponse = $client->Get("buckets/" . self::$RUNSCOPE_BUCKET . "/messages?count=20");
-      $this->assertTrue($messageResponse != NULL);
+      $this->assertTrue(null != $messageResponse);
 
       $data = json_decode($messageResponse, true);
 
       $messages = array();
       foreach ($data["data"] as $item) {
         $response = $client->Get("buckets/" . self::$RUNSCOPE_BUCKET . "/messages/" . $item["uuid"]);
-        $this->assertTrue($response != NULL);
+        $this->assertTrue(null != $response);
 
         $content2 = json_decode($response, true);
         array_push($messages, $content2['data']['request']['body']);
@@ -133,8 +125,9 @@ class AnalyticsE2ETest extends PHPUnit_Framework_TestCase {
       $count = 0;
       foreach ($messages as $m) {
         $msg = json_decode($m, true);
-        if (isset($msg['property']['id']) && $msg['property']['id'] == $this->id)
+        if (isset($msg['property']['id']) && $msg['property']['id'] == $this->id) {
           return;
+        }
       }
 
       sleep(5);
@@ -142,5 +135,18 @@ class AnalyticsE2ETest extends PHPUnit_Framework_TestCase {
 
     $this->assertTrue(false);
   }
+
+  private static function messageId()
+  {
+    return sprintf("%04x%04x%04x%04x%04x%04x%04x%04x",
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0x0fff) | 0x4000,
+      mt_rand(0, 0x3fff) | 0x8000,
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff),
+      mt_rand(0, 0xffff)
+    );
+  }
 }
-?>
--- test/ConsumerLibCurlTest.php
+++ test/ConsumerLibCurlTest.php
@@ -1,98 +1,109 @@
 <?php
 
-require_once(dirname(__FILE__) . "/../lib/Segment/Client.php");
+require_once __DIR__ . "/../lib/Segment/Client.php";
 
-class ConsumerLibCurlTest extends PHPUnit_Framework_TestCase {
-
+class ConsumerLibCurlTest extends PHPUnit\Framework\TestCase
+{
   private $client;
 
-  function setUp() {
+  public function setUp()
+  {
     date_default_timezone_set("UTC");
-    $this->client = new Segment_Client("oq0vdlg7yi",
-                          array("consumer" => "lib_curl",
-                                "debug"    => true));
-  }
-
-  function testTrack() {
-      $this->assertTrue($this->client->track(array(
-        "userId" => "lib-curl-track",
-        "event" => "PHP Lib Curl'd\" Event"
-      )));
-  }
-
-  function testIdentify() {
-    $this->assertTrue($this->client->identify(array(
-      "userId" => "lib-curl-identify",
-      "traits"  => array(
-        "loves_php" => false,
-        "type" => "consumer lib-curl test",
-        "birthday" => time()
+    $this->client = new Segment_Client(
+      "oq0vdlg7yi",
+      array(
+        "consumer" => "lib_curl",
+        "debug" => true,
       )
+    );
+  }
+
+  public function testTrack()
+  {
+    $this->assertTrue($this->client->track(array(
+      "userId" => "lib-curl-track",
+      "event" => "PHP Lib Curl'd\" Event",
     )));
   }
 
-  function testGroup(){
+  public function testIdentify()
+  {
+    $this->assertTrue($this->client->identify(array(
+      "userId" => "lib-curl-identify",
+      "traits" => array(
+        "loves_php" => false,
+        "type" => "consumer lib-curl test",
+        "birthday" => time(),
+      ),
+    )));
+  }
+
+  public function testGroup()
+  {
     $this->assertTrue($this->client->group(array(
       "userId" => "lib-curl-group",
       "groupId" => "group-id",
       "traits" => array(
-        "type" => "consumer lib-curl test"
-      )
+        "type" => "consumer lib-curl test",
+      ),
     )));
   }
 
-  function testPage(){
+  public function testPage()
+  {
     $this->assertTrue($this->client->page(array(
       "userId" => "lib-curl-page",
       "name" => "analytics-php",
       "category" => "fork-curl",
       "properties" => array(
-        "url" => "https://a.url/"
-      )
+        "url" => "https://a.url/",
+      ),
     )));
   }
 
-  function testScreen(){
+  public function testScreen()
+  {
     $this->assertTrue($this->client->page(array(
       "anonymousId" => "lib-curl-screen",
       "name" => "grand theft auto",
       "category" => "fork-curl",
-      "properties" => array()
+      "properties" => array(),
     )));
   }
 
-
-  function testAlias() {
+  public function testAlias()
+  {
     $this->assertTrue($this->client->alias(array(
       "previousId" => "lib-curl-alias",
-      "userId" => "user-id"
+      "userId" => "user-id",
     )));
   }
 
-  function testLargeMessageSizeError () {
+  public function testLargeMessageSizeError()
+  {
     $options = array(
-      "debug"    => true,
-      "consumer" => "lib_curl"
+      "debug" => true,
+      "consumer" => "lib_curl",
     );
 
     $client = new Segment_Client("testlargesize", $options);
 
     $big_property = "";
 
-    for ($i = 0; $i < 32 * 1024; $i++) {
+    for ($i = 0; $i < 32 * 1024; ++$i) {
       $big_property .= "a";
     }
 
-    $this->assertFalse($client->track(array(
-      "userId" => "some-user",
-      "event" => "Super Large PHP Event",
-      "properties" => array("big_property" => $big_property)
-    )) && $client->flush());
+    $this->assertFalse(
+      $client->track(
+        array(
+          "userId" => "some-user",
+          "event" => "Super Large PHP Event",
+          "properties" => array("big_property" => $big_property),
+        )
+      ) && $client->flush()
+    );
 
     $client->__destruct();
   }
-
-
 }
-
-?>
